9fd3cf963e8e99d24232b69bdd2ef43dc6d65b80
Tim Edwards <qwertyqwer.teamhavok@gmail.com>
2019-09-16 20:08:29 +1200

    Base functionality complete.
    Images load correctly at bottom with titles. No double ups, or missing 
    images. Clicking on an image now pops up a display showing a larger 
    image and more info on the movie, this also works correctly.

--- js/animate.js
+++ js/animate.js
@@ -1,8 +1,34 @@
-function toggleInfo() {
-    $("#grey").fadeToggle(anispeed);
-    $(".resultFocus").toggleClass("focusShown");
+var resultDisplayed = false;
+
+//Toggles the "Expanded" result, manages the visual function calls and the rss function calls
+//It by default will show a message such as "[blank] not found" for each value
+//When called (to open) it will call setFocusResult() to change these values to be the data in the movie arrays then call displayInfo() and show it on screen
+//Then when called again (to close) it will call displayInfo() to hide it, and set the values back to the "[blank] not found"
+function resultExpand(id) {
+    console.log("Result Expand Run");
+    if (resultDisplayed === true) {//If the result is already expanded
+        resetFocusResult();
+        displayInfo();
+        resultDisplayed = false;
+    }else if (resultDisplayed === false){//If the result not yet expanded
+        //Sets the result, waits for it to be setbefore continuing
+        console.log("result not displayed yet");
+        setFocusResult(id);
+        displayInfo();
+
+        resultDisplayed = true;
+    }else {//Something went terribly wrong
+        message("toggleInfo", "resultDisplayed returned incorrect value", "m");
+    }
 }
 
+function displayInfo() {
+    $("#grey").fadeToggle(anispeed);
+    $(".resultFocus").toggleClass("focusShown");
+    $(".resultFocus").slideToggle(anispeed);
+}
+
+/*
 function resultExpand(id) {
     toggleInfo();
     var easy = `
@@ -10,3 +36,4 @@ function resultExpand(id) {
     `;
     $('body').append('');
 }
+*/
