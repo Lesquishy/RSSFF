f3d952c3b171de9d5a0a50f38bbb33ff37e9d47b
Tim Edwards <qwertyqwer.teamhavok@gmail.com>
2019-09-19 00:14:48 +1200

    Focus.js added, now both searches work

--- js/animate.js
+++ js/animate.js
@@ -2,34 +2,6 @@
 
 var resultDisplayed = false;
 
-//Toggles the "Expanded" result, manages the visual function calls and the rss function calls
-//It by default will show a message such as "[blank] not found" for each value
-//When called (to open) it will call setFocusResult() to change these values to be the data in the movie arrays then call displayInfo() and show it on screen
-//Then when called again (to close) it will call displayInfo() to hide it, and set the values back to the "[blank] not found"
-function resultExpand(id) {
-    console.log("Result Expand Run");
-    if (resultDisplayed === true) {//If the result is already expanded
-        resetFocusResult();
-        displayInfo();
-        resultDisplayed = false;
-    }else if (resultDisplayed === false){//If the result not yet expanded
-        //Sets the result, waits for it to be setbefore continuing
-        console.log("result not displayed yet");
-        setFocusResult(id);
-        displayInfo();
-
-        resultDisplayed = true;
-    }else {//Something went terribly wrong
-        message("toggleInfo", "resultDisplayed returned incorrect value", "m");
-    }
-}
-
-function displayInfo() {
-    $("#grey").fadeToggle(anispeed);
-    $(".resultFocus").toggleClass("focusShown");
-    $(".resultFocus").slideToggle(anispeed);
-}
-
 function toggleSearchLoad() {
 
     $(".searchLoading").slideToggle(200);
@@ -38,9 +10,9 @@ function toggleSearchLoad() {
 
 async function changeSearch(id) {
     $(".searchNull").fadeOut(0);
-    if (id == 1) {
+    if (id == "tab1") {
 
-    }else if (id == 2) {
+    }else if (id == "tab2") {
         swipe();
         if (reSearch == null) {
             //display please search something
@@ -63,19 +35,19 @@ async function changeSearch(id) {
                 toggleSearchLoad();
             }, 410);
         }
-    }else if (id == 3){
+    }else if (id == "tab3"){
         toggleSearchLoad();
         setTimeout(function() {
             console.log("trsttegegeg");
             readSources(sourceFile);
         }, 200);
     }
-    if ($("#" + id + "").hasClass("active") == true) {
+    if ($(id).hasClass("active") == true) {
 
-    }else if (id == 3) {
+    }else if (id == "tab3") {
         console.log("should be colored");
         $(".active").removeClass("active");
-        $("#" + id + "").addClass("active");
+        $("#tab3").addClass("active");
         $(".blockOverflow").addClass("activated");
 
     }else {
